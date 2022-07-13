88fc94013549249d57a4703348ca6a3b2739c913
Assad Isah <nottherealalanturing@gmail.com>
2022-07-13 21:01:08 +0100

    feat: init commit

---
+++ syntax.rb
@@ -0,0 +1,44 @@
+if condition
+    do something
+elsif 
+    do something
+else
+
+end
+
+=begin 
+    opposite of unless
+=end
+if truecondition
+    do something
+end
+
+=begin 
+    oposite of if !true
+=end
+unless falsecondition
+    do something
+end
+
+=begin 
+    dont use
+=end
+loop do
+    do something
+    break if condition
+end
+
+=begin 
+    do while loop
+=end
+while truecondition do
+    do something
+end
+
+=begin 
+    opposite of do while
+=end
+until falsecondition
+    do something
+end
+
